216498bfcd6886a2a3ce9dfd4e24d91cedd4db3f
David-Yanez <luis.yanez@epn.edu.ec>
2024-05-31 21:18:54 -0500

    category pages added

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,43 @@
+import { ProductGrid, Title } from "@/components"
+import { Categoryy } from "@/interfaces"
+import { initialData } from "@/seed/seed"
 import { notFound } from "next/navigation"
+
+const seedProducts = initialData.products
 
 interface Props{
     params:{
-        id: string
+        id: Categoryy
     } 
 }
 
 export default function Category ({params}: Props){
 
-    const {id} = params
-    if(id === 'kids'){
+   const {id} = params
+   const products = seedProducts.filter(product => product.gender === id)
+  /*  if(id === 'kids'){
         notFound()
+    }
+*/
+    const labels: Record<Categoryy, string> = {
+        'men': 'para Hombres',
+        'women': 'para Mujeres',
+        'kid': 'para Niños',
+        'unisex': 'para Todos'
     }
 
     return(
-        <div>
-            <h1>Category page {id}</h1>
-        </div>
+        <>
+          
+            <Title
+      title={`Articulos de ${labels[id]}`}
+      subtitle="Todos los productos"
+      className="mb-2"
+      />
+
+      <ProductGrid
+      products={products}
+      />
+        </>
     )
 }
