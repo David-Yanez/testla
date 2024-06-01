63ddc4f072a01548237bdc8a7523e80ad7354749
David-Yanez <luis.yanez@epn.edu.ec>
2024-05-31 23:12:28 -0500

    product page created

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,46 @@
-export default function (){
+import { titleFont } from "@/config/fonts"
+import { initialData } from "@/seed/seed"
+import { notFound } from "next/navigation"
+
+
+interface Props {
+    params: {
+        slug: string
+    }
+}
+
+/* eslint import/no-anonymous-default-export: [2, {"allowAnonymousFunction": true}] */
+export default function ProductSlug({params}: Props) {
+    const {slug} = params
+    const product = initialData.products.find(product => product.slug === slug )
+
+    if(!product){
+        notFound()
+    }
+
     return(
-        <div>
-            <h1>Product Slug page </h1>
+        <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+            {/**Slideshow */}
+            <div className="col-span-1 md:col-span-2 ">
+                <h2>hola a todos</h2>
+            </div>
+            {/**Detalles */}
+            <div className="col-span-1 px-5 ">
+                <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
+                <p className="text-lg mb-5">{product.price}</p>
+                {/**Selector de tallas */}
+                {/**Selector de CAntidad  */}
+                <button className="btn-primary my-5">
+                    Agregar al carrito
+                </button>
+                {/**Descripción */}
+                <h3 className="font-bold text-sm">
+                    Descripción
+                </h3>
+                <p className="font-light">
+                    {product.description}
+                </p>
+            </div>
         </div>
     )
 }
